17f630e774aa8edebc3fba4ab091b957cfb75bc6
Myokin <maiyaokeng888@163.com>
2025-06-01 23:10:00 +0800

    second commit

--- src/main/java/com/myokin/git/GitTest.java
+++ src/main/java/com/myokin/git/GitTest.java
@@ -3,5 +3,7 @@ package com.myokin.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println( "Hello git!");
+         System.out.println( "Hello git2!");
+          System.out.println( "Hello git3!");
     }
 }
