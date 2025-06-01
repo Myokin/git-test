52431372ee9388e9b9a081210e0241dd869d20d7
Myokin <maiyaokeng888@163.com>
2025-06-01 23:49:08 +0800

    hot-fix2 commit

--- src/main/java/com/myokin/git/GitTest.java
+++ src/main/java/com/myokin/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
          System.out.println( "Hello git2!");
           System.out.println( "Hello git3!");
         System.out.println( "hot-fix test!");
+        System.out.println( "hot-fix test2!");
 
     }
 }
